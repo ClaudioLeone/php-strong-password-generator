f9a7038aef300df4e067f97ddd2107ed3993e8c1
ClaudioLeone <claleone92@gmail.com>
2023-05-06 00:05:46 +0200

    Created and linked 'utilities' css doc for better styles organization, created pwd generator basic page

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    $test_text = "TEST";
+    
 ?>
 
 <!DOCTYPE html>
@@ -9,11 +9,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="CSS/utilities.css">
 </head>
 
 <body>
-    <div>
-        <?php echo $test_text; ?>
-    </div>
+    <header>
+        <div class="top-text-container">
+            <h1>Strong Password Generator</h1>
+            <h2>Genera una password sicura</h2>
+        </div>
+    </header>
+
+    <main>
+        <div class="form-container flex f-col f-justify-between">
+            <form class="form-primary flex f-justify-between" action="" method="get">
+                <label for="text">Lunghezza pwd da generare:</label>
+
+                <div class="inputs flex f-align-center f-justify-between">
+                    <input type="text" name="text" id="text">
+                    <input class="btn-reset" type="reset" value="❌">
+                </div>
+            </form>
+        </div>
+    </main>
 </body>
 </html>
